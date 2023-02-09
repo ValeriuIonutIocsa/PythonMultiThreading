80a4fe0388417d2f5445761fcf0887326b7054d5
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2023-02-09 10:31:15 +0200

    first commit

---
+++ multi_threading.py
@@ -0,0 +1,6 @@
+import multi_threading1
+
+mode = 1
+
+if mode == 1:
+    multi_threading1.work()
---
+++ multi_threading1.py
@@ -0,0 +1,52 @@
+import threading
+import time
+import sys
+
+
+class WallPainter:
+
+    def __init__(self, wallIndex):
+        self.wallIndex = wallIndex
+
+    def paintwall(self):
+        time.sleep(1)
+        sys.stdout.write('Wall ' + str(self.wallIndex) + ' painted\n')
+
+
+def work():
+    wall_painter_list = [
+        WallPainter(1),
+        WallPainter(2),
+        WallPainter(3),
+        WallPainter(4)
+    ]
+    work_single_threaded(wall_painter_list)
+    work_multi_threaded(wall_painter_list)
+
+
+def work_single_threaded(wall_painter_list):
+    sys.stdout.write('\nworking single-threaded\n')
+    start_time = time.time()
+
+    for wall_painter in wall_painter_list:
+        wall_painter.paintwall()
+
+    exec_time = str(round(time.time() - start_time, 2))
+    sys.stdout.write('\ndone; execution time: ' + exec_time + ' seconds\n')
+
+
+def work_multi_threaded(wall_painter_list):
+    sys.stdout.write('\nworking multi-threaded\n')
+    start_time = time.time()
+
+    thread_list = []
+    for wall_painter in wall_painter_list:
+        thread = threading.Thread(target=wall_painter.paintwall)
+        thread_list.append(thread)
+        thread.start()
+
+    for thread in thread_list:
+        thread.join()
+
+    exec_time = str(round(time.time() - start_time, 2))
+    sys.stdout.write('\ndone; execution time: ' + exec_time + ' seconds\n')
