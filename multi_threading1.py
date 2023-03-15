8b3254901b24afcdfe99b6bd1a0154aa5ce68a24
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2023-03-15 13:43:46 +0200

    small improvement

--- multi_threading1.py
+++ multi_threading1.py
@@ -8,7 +8,7 @@ class WallPainter:
     def __init__(self, wallIndex):
         self.wallIndex = wallIndex
 
-    def paintwall(self):
+    def paint_wall(self):
         time.sleep(1)
         sys.stdout.write('Wall ' + str(self.wallIndex) + ' painted\n')
 
@@ -29,7 +29,7 @@ def work_single_threaded(wall_painter_list):
     start_time = time.time()
 
     for wall_painter in wall_painter_list:
-        wall_painter.paintwall()
+        wall_painter.paint_wall()
 
     exec_time = str(round(time.time() - start_time, 2))
     sys.stdout.write('\ndone; execution time: ' + exec_time + ' seconds\n')
@@ -41,7 +41,7 @@ def work_multi_threaded(wall_painter_list):
 
     thread_list = []
     for wall_painter in wall_painter_list:
-        thread = threading.Thread(target=wall_painter.paintwall)
+        thread = threading.Thread(target=wall_painter.paint_wall)
         thread_list.append(thread)
         thread.start()
 
