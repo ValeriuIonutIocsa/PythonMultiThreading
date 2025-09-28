2133ab4b5e6df8191e7cb4d3f19e70aff841d7df
Iocsa, Valeriu-Ionut <valeriu.iocsa@vitesco.com>
2025-09-28 13:17:40 +0300

    added main function

--- multi_threading.py
+++ multi_threading.py
@@ -1,6 +1,11 @@
 import multi_threading1
 
-mode = 1
 
-if mode == 1:
-    multi_threading1.work()
+def main():
+    mode = 1
+    if mode == 1:
+        multi_threading1.work()
+
+
+if __name__ == "__main__":
+    main()
